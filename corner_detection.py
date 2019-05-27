032fc9d4681d354e81cf30ec517ad04f3101df7d
andremsouza <msouza.andre@hotmail.com>
2019-05-26 23:39:10 -0300

    DONE: Structure tensor setup

--- corner_detection.py
+++ corner_detection.py
@@ -28,6 +28,7 @@ def rgb_to_grayscale(img):
 	return np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
 
 # * Calculates x and y derivatives using Sobel operator
+# ? Study possibility of replacement with np.gradient
 def image_derivatives(arr, x = True, y = True):
 	kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
 	kernel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
@@ -35,12 +36,16 @@ def image_derivatives(arr, x = True, y = True):
 		scipy.signal.convolve2d(arr, kernel_y, mode='same') if y else None]
 
 # ! Remove this function after done testing
-# Calculate gradient of image array
+# * Calculates gradient of image array
 def image_gradient(arr):
 	deriv = image_derivatives(arr)
 	return np.power(np.power(deriv[0], 2) + np.power(deriv[1], 2), 1/2)
 
-# TODO: Structure tensor setup
+# * Calculates and returns tensor setup, from image derivatives
+# * Important to remember: i_x * i_y == i_y * i_x
+def tensor_setup(i_x, i_y):
+	return [i_x ** 2, i_x * i_y, i_y **2]
+
 # TODO: Harris response calculation
 # TODO: Find edges and corners using R
 
