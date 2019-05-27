57af93fc42baa17a63753ee58f40991123bb397a
andremsouza <msouza.andre@hotmail.com>
2019-05-26 23:28:16 -0300

    DONE: Spatial derivative calculation

--- corner_detection.py
+++ corner_detection.py
@@ -6,6 +6,7 @@ import imageio
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import scipy
+import scipy.signal
 #%% [markdown]
 # ## Visualizing examples
 #%%
@@ -22,12 +23,22 @@ plt.show()
 #%% [markdown]
 # ## Implementing Harris Corner Detector
 #%%
-# * This function converts an RGB image to gray scale
-# * Using the ITU-R 601-2 luma transform
+# * Converts an RGB image to gray scale, using the ITU-R 601-2 luma transform
 def rgb_to_grayscale(img):
-    return np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
+	return np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
 
-# TODO: Spatial derivative calculation
+# * Calculates x and y derivatives using Sobel operator
+def image_derivatives(arr, x = True, y = True):
+	kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+	kernel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+	return [scipy.signal.convolve2d(arr, kernel_x, mode='same') if x else None, \
+		scipy.signal.convolve2d(arr, kernel_y, mode='same') if y else None]
+
+# ! Remove this function after done testing
+# Calculate gradient of image array
+def image_gradient(arr):
+	deriv = image_derivatives(arr)
+	return np.power(np.power(deriv[0], 2) + np.power(deriv[1], 2), 1/2)
 
 # TODO: Structure tensor setup
 # TODO: Harris response calculation
