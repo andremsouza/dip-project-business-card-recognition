93fb90d2b9dfc1ff5df3ba84af4750863118e916
andremsouza <msouza.andre@hotmail.com>
2019-05-28 23:45:24 -0300

    Remove testing cell

--- corner_detection.py
+++ corner_detection.py
@@ -8,19 +8,6 @@ import matplotlib as mpl
 import scipy
 import skimage
 #%% [markdown]
-# ## Visualizing examples
-#%%
-# ! Remove cell after finishing initial tests
-img1 = imageio.imread("./images/ex1.jpg")
-img2 = imageio.imread("./images/ex2.png")
-img3 = imageio.imread("./images/ex3.jpg")
-plt.figure(figsize=(16, 9))
-plt.subplot(131); plt.imshow(img1)
-plt.subplot(132); plt.imshow(img2)
-plt.subplot(133); plt.imshow(img3)
-plt.show()
-
-#%% [markdown]
 # ## Implementing Harris Corner Detector
 #%%
 # * Converts an RGB image to gray scale, using the ITU-R 601-2 luma transform
