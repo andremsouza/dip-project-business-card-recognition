9ffbf19ae679adaef5b9c2a0733b26fb57b725f8
andremsouza <msouza.andre@hotmail.com>
2019-06-13 15:15:42 -0300

    Finalized tests with openCV. Implementing self-made transform.

--- perspective_transform.py
+++ perspective_transform.py
@@ -3,6 +3,60 @@
 import numpy as np
 import cv2
 from corner_detection import CornerDetector
+# %%
+
+
+def four_point_transform(img, src, dst):
+    """ Transform img, using four points in src, to four points in dst
+    # TODO: Docstring
+    """
+    pass
+
+
+def four_point_transform_cv2(image, pts):
+    """ Transform img according to the 4 points in pts
+
+    Note:
+        It is assumed that the points in pts are in the following order:
+        [top-left, top-right, bottom-left, bottom-right]
+    # TODO: Docstring
+    """
+    rect = pts.astype(np.float32)
+    (tl, tr, bl, br) = pts
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [0, maxHeight - 1],
+        [maxWidth - 1, maxHeight - 1],
+    ], dtype=np.float32)
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
+
 
 # %%
 # Running tests on an random image
@@ -18,29 +72,16 @@ if __name__ == "__main__":
     ]
     # Selecting random file for testing
     file_img = example_files[np.random.randint(0, len(example_files))]
+    # file_img = './images/806123698_321554.jpg'  # Good file for testing
     img = imageio.imread(file_img)
 
     # Finding corners from input image
-    corner_points = CornerDetector(img).find_corners4()
-    # new_corners = np.array(
-    #     [[corner_points[:, 0].min(), corner_points[:, 1].min()],
-    #      [corner_points[:, 0].min(), corner_points[:, 1].max()],
-    #      [corner_points[:, 0].max(), corner_points[:, 1].min()],
-    #      [corner_points[:, 0].max(), corner_points[:, 1].max()]])
-    new_corners = np.array([
-        [0, 0],
-        [0, corner_points[:, 1].max() - corner_points[:, 1].min() - 1],
-        [corner_points[:, 0].max() - corner_points[:, 0].min() - 1, 0],
-        [
-            corner_points[:, 0].max() - corner_points[:, 0].min() - 1,
-            corner_points[:, 1].max() - corner_points[:, 1].min() - 1
-        ],
-    ])
-
-    matrix = cv2.getPerspectiveTransform(corner_points, new_corners)
-    result = cv2.warpPerspective(
-        img, matrix, (corner_points[:, 0].max() - corner_points[:, 0].min(),
-                      corner_points[:, 1].max() - corner_points[:, 1].min()))
-    plt.figure(figsize=(15, 15))
+    corner_points = CornerDetector(img).find_corners4().astype(np.float32)
+    corner_points[:, [0, 1]] = corner_points[:, [1, 0]]
+    img2 = cv2.imread(file_img)
+    result = four_point_transform_cv2(img2, corner_points)
+    plt.figure(figsize=(10, 10))
     plt.imshow(result)
     plt.show()
+    # cv2.imwrite("./out.png", result)
+    # TODO: Test my implementation
